c9f8251674d39b1f40ca6bbdec85c7794f56d767
brunalauschner <brunalauschner@gmail.com>
2020-04-14 15:07:02 -0300

    Projeto finalizado

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <toDoForm :caixinhas="caixinhas" @addCaixa="addCaixa($event)"/>
+    <toDoList :caixinhas="caixinhas"/>
+  </div>
+</template>
+
+<script>
+import toDoForm from "./components/ToDoForm.vue"
+import toDoList from "./components/ToDoList.vue"
+
+export default {
+  name: 'App',
+  components: {
+    toDoForm, toDoList
+  }, 
+  data: function() {
+    return{
+      caixinhas: [{
+        titulo: "Estudar"
+      },{
+        titulo: "Jogar"
+      }
+      ]
+    }
+  }, methods:{
+    addCaixa: function(event){
+      this.caixinhas.push({titulo: event.titulo})
+    }
+    }
+  }
+</script>
+
+<style>
+</style>
---
+++ src/components/ToDoList.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="caixa">
+      <h5 id="titu">Caixas:</h5>
+      <div class="caixinhas" v-for="(caixa, index) in caixinhas" :key = "index" >
+          {{caixa.titulo}}
+      </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+ name: "Caixas",
+ props: ['caixinhas']
+}
+</script>
+
+
+<style>
+#caixa{
+    width: 450px;
+    height: 200px;
+    border: 1px solid #FFF;
+    margin: 30px auto 10px auto;
+    display: flex;
+    flex-wrap: wrap;
+}
+.caixinhas{
+    width: 70px;
+    height: 50px;
+    border: 1px solid #FFF;
+    margin: 30px 20px 0px 0px;
+    text-align: center;
+    line-height: 50px;
+    user-select: none;
+    cursor: pointer;
+    background-color: burlywood;
+}
+#titu{
+    margin: 5px 0 0 5px;
+}
+</style>
